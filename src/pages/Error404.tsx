2d2a88938fedbc5faa447218f6be60555ba502cb
thomasop <tdss33@hotmail.com>
2023-02-23 16:58:57 +0100

    Add: Error page and style

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Footer from "../conponents/Footer";
 import Header from "../conponents/Header";
 
@@ -6,7 +7,13 @@ const Error404: React.FC = () => {
   return (
     <>
       <Header />
-      <div>Error404</div>
+      <main>
+        <section className="error">
+          <h1 className="error__h1">404</h1>
+          <p className="error__p">Oups ! La page que vous demandez n'existe pas.</p>
+          <Link className="error__a" to="/">Retourner sur la page d’accueil</Link>
+        </section>
+      </main>
       <Footer />
     </>
   );
